119bc2be6d15e2b2cfa8c755475b620e9ca1c960
foddom520 <115153461+foddom520@users.noreply.github.com>
2025-10-08 10:35:48 +0200

    backend v2

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mysql = require('mysql2');
 const app = express();
+import 
 
 const PORT = process.env.PORT || 3000
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'Root',
-    password:'',
-    database:'fogado'
+    host:process.env.host || 'localhost',
+    user: process.env.user || 'root',
+    password: process.env.password || '',
+    database: process.env.database || 'fogado'
 });
 
 db.connect(err => {
@@ -27,8 +28,8 @@ app.get('/szobak', (req, res) => {
     });
 });
 
-app.get('/szobak', (req, res) => {
-    const sql = 'SELECT szobak.sznev, agy, potagy FROM szobak;';
+app.get('/fogalalsok', (req, res) => {
+    const sql = 'SELECT SUM(foglalasok.fo) AS vendegek FROM foglalasok;';
     db.query(sql, (err, results) => {
         if (err) {
             console.error("sql error:", err);
@@ -38,8 +39,19 @@ app.get('/szobak', (req, res) => {
     });
 });
 
-app.get('/szobak', (req, res) => {
-    const sql = 'SELECT szobak.sznev, agy, potagy FROM szobak;';
+app.get('/fogalalsok', (req, res) => {
+    const sql = 'SELECT DATEDIFF(foglalasok.tav, foglalasok.erk) as vendegejszakak FROM foglalasok;';
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error("sql error:", err);
+            return res.status(500).send('database error');
+        }
+        res.json(results);
+    });
+});
+
+app.get('/fogalalsok', (req, res) => {
+    const sql = 'SELECT vendegek.vnev, foglalasok.szoba, foglalasok.erk, foglalasok.tav FROM foglalasok AS foglalasok JOIN vendegek AS vendegek ON foglalasok.vendeg = vendegek.vsorsz;';
     db.query(sql, (err, results) => {
         if (err) {
             console.error("sql error:", err);
